27dde2aa27eefea4d3677f92e1367a7a6a9db59b
Laurent Destailleur <eldy@destailleur.fr>
2020-02-07 17:02:20 +0100

    Creation of tgz even if not a git dir

--- scripts/git_update_sources.sh
+++ scripts/git_update_sources.sh
@@ -12,43 +12,45 @@ if [ "x$1" == "x" ]; then
    exit 1
 fi
 
-echo "Update git dirs found into $1"
+echo "Update git dirs found into $1 and generate the tgz image."
 
-#for dir in `find $1 -type d`
-for dir in `ls -d $1/* | grep -v tar.gz`
+for dir in `ls -d $1/* | grep -v tgz`
 do
     echo -- Process dir $dir
-    if [ -d "$dir/.git" ]; then
-        cd $dir
-        git reset --hard HEAD
-        # Do not use git pull --depth=1 here, this will maka merge errors.
-        git pull
-        echo Result of git pull = $?
-   
-    	echo "Clean some dirs to save disk spaces"
-    	rm -fr documents/*
-    	rm -fr dev/ test/ doc/ htdocs/includes/ckeditor/ckeditor/adapters htdocs/includes/ckeditor/ckeditor/samples
-    	rm -fr htdocs/includes/sabre/sabre/*/tests htdocs/includes/stripe/tests
-    	rm -fr htdocs/includes/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-* htdocs/includes/tecnickcom/tcpdf/fonts/freefont-* htdocs/includes/tecnickcom/tcpdf/fonts/ae_fonts_*
-    	#rm -fr vendor/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-* vendor/tecnickcom/tcpdf/fonts/freefont-* vendor/tecnickcom/tcpdf/fonts/ae_fonts_*
-    	rm -fr files/_cache/*
-    	# We remove subdir of build. We need files.
-    	find build/* -type d -exec rm -fr {} \;
-    	
-        if [ -s build/generate_filelist_xml.php ]; then
-                echo "Found generate_filelist_xml.php"
-                php build/generate_filelist_xml.php release=auto-dolicloud
-        fi
-    
-    	# Create a deployment tar file
-    	echo "Compress the repository into an archive $dir.tar.gz"
+    cd $dir
+	if [ $? -eq 0 ]; then
 		export gitdir=`basename $dir`
-    	tar cz --exclude-vcs -f $dir/../$gitdir.tgz .
-
-        cd -
-    else
-        echo "Not a git dir. Nothing done."
-    fi
+	
+	    if [ -d ".git" ]; then
+	        git reset --hard HEAD
+	        # Do not use git pull --depth=1 here, this will maka merge errors.
+	        git pull
+	        echo Result of git pull = $?
+	    else
+	        echo "Not a git dir. Nothing done."
+	    fi
+	   
+		echo "Clean some dirs to save disk spaces"
+		rm -fr documents/*
+		rm -fr dev/ test/ doc/ htdocs/includes/ckeditor/ckeditor/adapters htdocs/includes/ckeditor/ckeditor/samples
+		rm -fr htdocs/includes/sabre/sabre/*/tests htdocs/includes/stripe/tests
+		rm -fr htdocs/includes/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-* htdocs/includes/tecnickcom/tcpdf/fonts/freefont-* htdocs/includes/tecnickcom/tcpdf/fonts/ae_fonts_*
+		#rm -fr vendor/tecnickcom/tcpdf/fonts/dejavu-fonts-ttf-* vendor/tecnickcom/tcpdf/fonts/freefont-* vendor/tecnickcom/tcpdf/fonts/ae_fonts_*
+		rm -fr files/_cache/*
+		# We remove subdir of build. We need files.
+		find build/* -type d -exec rm -fr {} \;
+		
+	    if [ -s build/generate_filelist_xml.php ]; then
+	            echo "Found generate_filelist_xml.php"
+	            php build/generate_filelist_xml.php release=auto-dolicloud
+	    fi
+	
+		# Create a deployment tar file
+		echo "Compress the repository into an archive $dir.tar.gz"
+		tar cz --exclude-vcs -f $dir/../$gitdir.tgz .
+	
+	    cd -
+	fi
 done
 
 echo "Finished."
