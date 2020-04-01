1ada2363f67058938df2695b6a6aa3dbc3d3d5b1
Laurent Destailleur <eldy@destailleur.fr>
2020-04-01 12:36:48 +0200

    Switch ipv6

---
+++ scripts/switchipv6.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+if [ "x$1" = "xdisable" ] ;
+then
+echo Disable IPV6
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.all.autoconf=0
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.autoconf=0
+ifconfig
+more /proc/net/if_inet6
+fi
+
+if [ "x$1" = "xenable" ] ;
+then
+echo Enable IPV6
+sysctl -w net.ipv6.conf.default.autoconf=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=0
+sysctl -w net.ipv6.conf.all.autoconf=1
+sysctl -w net.ipv6.conf.all.disable_ipv6=0
+ifconfig
+more /proc/net/if_inet6
+fi
