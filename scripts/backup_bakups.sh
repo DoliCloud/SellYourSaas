1708125327b9249beafaa00c6dac952371db7efe
Laurent Destailleur <eldy@destailleur.fr>
2020-05-01 23:28:14 +0200

    option rsync

--- scripts/backup_bakups.sh
+++ scripts/backup_bakups.sh
@@ -42,7 +42,7 @@ export EMAILFROM=support@$DOMAIN
 export EMAILTO=supervision@$DOMAIN
 #export OPTIONS="-v --stats -a --delete";
 #export OPTIONS="-v --stats -a --delete --delete-excluded";
-export OPTIONS="-v --stats -a";
+export OPTIONS="-v --stats -rltgoD";
 
 echo >> /var/log/backup_backups.log
 
