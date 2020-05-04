b47b55342a3b4ffb5c93ef7d9a8f4d5072bb94dd
Laurent Destailleur <eldy@destailleur.fr>
2020-05-04 14:50:37 +0200

    Debug backu_backups

--- scripts/backup_backups.sh
+++ scripts/backup_backups.sh
@@ -29,14 +29,14 @@ export scriptdir=$(dirname $(realpath ${0}))
 export DOMAIN=`grep '^domain=' /etc/sellyoursaas.conf | cut -d '=' -f 2`
 
 # Source
-export $DIRSOURCE1="/home";
-export $DIRSOURCE2=`grep 'backupdir=' /etc/sellyoursaas.conf | cut -d '=' -f 2`
+export DIRSOURCE1="/home";
+export DIRSOURCE2=`grep 'backupdir=' /etc/sellyoursaas.conf | cut -d '=' -f 2`
 
 # Target
-export $SERVDESTI=`grep 'remotebackupserver=' /etc/sellyoursaas.conf | cut -d '=' -f 2`
-export $USER="admin";
-export $DIRDESTI1="/mnt/diskbackup/home_"`hostname`;
-export $DIRDESTI2="/mnt/diskbackup/backup_"`hostname`;
+export SERVDESTI=`grep 'remotebackupserver=' /etc/sellyoursaas.conf | cut -d '=' -f 2`
+export USER="admin";
+export DIRDESTI1="/mnt/diskbackup/home_"`hostname`;
+export DIRDESTI2="/mnt/diskbackup/backup_"`hostname`;
 
 export EMAILFROM=support@$DOMAIN
 export EMAILTO=supervision@$DOMAIN
@@ -47,13 +47,24 @@ export OPTIONS="-v --stats -rlt --noatime --backup --suffix=.old";
 echo >> /var/log/backup_backups.log
 
 echo "DOMAIN=$DOMAIN" >> /var/log/backup_backups.log
+echo "DIRSOURCE1=$DIRSOURCE1" >> /var/log/backup_backups.log
+echo "DIRSOURCE2=$DIRSOURCE2" >> /var/log/backup_backups.log
+echo "SERVDESTI=$SERVDESTI" >> /var/log/backup_backups.log
 echo "EMAILFROM=$EMAILFROM" >> /var/log/backup_backups.log
 echo "EMAILTO=$EMAILTO" >> /var/log/backup_backups.log
 echo "PID=$PID" >> /var/log/backup_backups.log
 
+echo "DOMAIN=$DOMAIN"
+echo "DIRSOURCE1=$DIRSOURCE1"
+echo "DIRSOURCE2=$DIRSOURCE2"
+echo "SERVDESTI=$SERVDESTI"
+echo "EMAILFROM=$EMAILFROM"
+echo "EMAILTO=$EMAILTO"
+echo "PID=$PID"
+
 
 echo "**** ${0} started" >> /var/log/backup_backups.log
-echo $now" Start to copy backups ofn a remote server" >> /var/log/backup_backups.log 
+echo $now" Start to copy backups on a remote server" >> /var/log/backup_backups.log 
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
@@ -82,7 +93,8 @@ $command >>/var/log/backup_backups.log
 export ret1=$?
 
 echo "Do a rsync for second part"
-foreach i in {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}; do
+
+for i in {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}; do
         export command="rsync -x --exclude '*_log' --exclude '*.log' --exclude '*log.*.gz' --exclude '_sessions/*' --exclude '_log/*' --exclude '_tmp/*' -e ssh $OPTIONS $DIRSOURCE2/osu$i* $USER\@$SERVDESTI:$DIRDESTI2";
         echo "$command\n";
 
