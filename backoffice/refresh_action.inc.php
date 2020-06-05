1882dc6639a4f4db2dd876824a67717b381c3e80
Laurent Destailleur <eldy@destailleur.fr>
2020-06-05 17:50:45 +0200

    Use the definition of service wit resource_label 'User' to count nb of users instead of hard coded SQL.

--- backoffice/refresh_action.inc.php
+++ backoffice/refresh_action.inc.php
@@ -256,7 +256,7 @@ if ($action == 'delinstalllock')
 }
 
 
-// We make a refresh of status of install.lock + authorized key, this does not update the qty (this is done in makeRenewal.
+// We make a refresh of status of install.lock + authorized key, this does not update the qty (this is done in makeRenewal).
 if ($action == 'refresh' || $action == 'setdate')
 {
     dol_include_once("/sellyoursaas/backoffice/lib/refresh.lib.php");		// do not use dol_buildpath to keep global of var into refresh.lib.php working
@@ -264,10 +264,10 @@ if ($action == 'refresh' || $action == 'setdate')
 	$object->oldcopy=dol_clone($object, 1);
 
 	// Setup files refresh (does not update lastcheck field)
-	$ret=dolicloud_files_refresh($conf,$db,$object,$errors);
+	$ret=dolicloud_files_refresh($conf, $db, $object, $errors);
 
 	// Database refresh (also update lastcheck field)
-	$ret=dolicloud_database_refresh($conf,$db,$object,$errors);
+	$ret=dolicloud_database_refresh($conf, $db, $object, $errors);
 
 	$action = 'view';
 }
