7832b906ec54424766a074d08262d6b47f406feb
Laurent Destailleur <eldy@destailleur.fr>
2020-06-24 10:50:08 +0200

    Add log

--- scripts/remote_server/index.php
+++ scripts/remote_server/index.php
@@ -142,6 +142,7 @@ if (in_array($tmparray[0], array('backup')))
 {
 	if ($DEBUG) fwrite($fh, date('Y-m-d H:i:s').' ./backup_instance.php '.$paramarray[2].'.'.$paramarray[3].' '.$backupdir." confirm\n");
 	else fwrite($fh, date('Y-m-d H:i:s').' ./backup_instance.php '.$paramarray[2].'.'.$paramarray[3].' '.$backupdir." confirm\n");
+	fwrite($fh, "getcwd() = ".getcwd()."\n");
 
 	exec('sudo -u admin ./backup_instance.php '.$paramarray[2].'.'.$paramarray[3].' '.$backupdir.' confirm 2>&1', $output, $return_var);
 
