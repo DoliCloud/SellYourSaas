34735f4da29914e7530bb209eb210c8f41cab33e
Laurent Destailleur <eldy@destailleur.fr>
2020-09-26 12:45:48 +0200

    css

--- myaccount/index.php
+++ myaccount/index.php
@@ -3771,7 +3771,9 @@ if ($mode == 'instances')
 						  // Instance status
 				          print '<span class="caption-helper floatright clearboth">';
 				          //print $langs->trans("Status").' : ';
-				          print '<!-- status = '.$statuslabel.' --><span class="bold uppercase" style="color:'.$color.'">';
+				          print '<!-- status = '.$statuslabel.' -->';
+
+				          print '<span class="bold uppercase" style="background-color:'.$color.'; border-radius: 5px; padding: 10px; color: #fff;">';
 				          if ($statuslabel == 'processing') print $langs->trans("DeploymentInProgress");
 				          elseif ($statuslabel == 'done') print $langs->trans("Alive");
 				          elseif ($statuslabel == 'suspended') print $langs->trans("Suspended").' '.img_warning();
