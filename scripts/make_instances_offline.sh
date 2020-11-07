c8ecfbe35b59635e5325a4838d888c748b3b4cac
Regis Houssin <regis.houssin@inodbox.com>
2020-11-07 17:29:07 +0100

    FIX wrong check operator

--- scripts/make_instances_offline.sh
+++ scripts/make_instances_offline.sh
@@ -9,15 +9,15 @@ echo "***** $0 *****"
 
 # possibility to change the ssl certificates name
 export webSSLCertificateCRT=`grep 'websslcertificatecrt=' /etc/sellyoursaas.conf | cut -d '=' -f 2`
-if [[ "x$webSSLCertificateCRT" != "x" ]]; then
+if [[ "x$webSSLCertificateCRT" == "x" ]]; then
 	export webSSLCertificateCRT=with.sellyoursaas.com.crt
 fi
 export webSSLCertificateKEY=`grep 'websslcertificatekey=' /etc/sellyoursaas.conf | cut -d '=' -f 2`
-if [[ "x$webSSLCertificateKEY" != "x" ]]; then
+if [[ "x$webSSLCertificateKEY" == "x" ]]; then
 	export webSSLCertificateKEY=with.sellyoursaas.com.key
 fi
 export webSSLCertificateIntermediate=`grep 'websslcertificateintermediate=' /etc/sellyoursaas.conf | cut -d '=' -f 2`
-if [[ "x$webSSLCertificateIntermediate" != "x" ]]; then
+if [[ "x$webSSLCertificateIntermediate" == "x" ]]; then
 	export webSSLCertificateIntermediate=with.sellyoursaas.com-intermediate.crt
 fi
 
