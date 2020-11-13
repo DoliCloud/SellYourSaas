f3671a894693a9465bbf6e0f2865ff49c081fc8a
Laurent Destailleur <eldy@destailleur.fr>
2020-11-13 15:56:56 +0100

    Menu bar is sticky in myaccount

--- myaccount/tpl/myaccount.tpl.php
+++ myaccount/tpl/myaccount.tpl.php
@@ -58,104 +58,118 @@ print '
 	      <div class="col-md-6">
 
 	        <div class="portlet light">
-          <div class="portlet-title">
-            <div class="caption-subject font-green-sharp bold uppercase">'.$langs->trans("Organization").'</div>
-          </div>
-          <div class="portlet-body">
+	          <div class="portlet-title">
+	            <div class="caption-subject font-green-sharp bold uppercase">'.$langs->trans("Organization").'</div>
+	          </div>
+	          <div class="portlet-body">
 
-            <form action="'.$_SERVER["PHP_SELF"].'" method="post" name="formsoc">
-            <input type="hidden" name="token" value="'.newToken().'">
-			<input type="hidden" name="action" value="updatemythirdpartyaccount">
-			<input type="hidden" name="mode" value="'.dol_escape_htmltag($mode).'">
+	            <form action="'.$_SERVER["PHP_SELF"].'" method="post" name="formsoc">
+	            <input type="hidden" name="token" value="'.newToken().'">
+				<input type="hidden" name="action" value="updatemythirdpartyaccount">
+				<input type="hidden" name="mode" value="'.dol_escape_htmltag($mode).'">
 
-              <div class="form-body">
+	              <div class="form-body">
 
-                <div class="form-group">
-                  <label>'.$langs->trans("NameOfCompany").'</label>
-                  <input type="text" class="form-control" placeholder="'.$langs->trans("NameOfYourOrganization").'" value="'.$mythirdpartyaccount->name.'" name="orgName">
-                </div>
+	                <div class="form-group">
+	                  <label>'.$langs->trans("NameOfCompany").'</label>
+	                  <input type="text" class="form-control" placeholder="'.$langs->trans("NameOfYourOrganization").'" value="'.$mythirdpartyaccount->name.'" name="orgName">
+	                </div>
 
-                <div class="form-group">
-                  <label>'.$langs->trans("AddressLine").'</label>
-                  <input type="text" class="form-control" placeholder="'.$langs->trans("HouseNumberAndStreet").'" value="'.$mythirdpartyaccount->address.'" name="address">
-                </div>
-                <div class="form-group">
-                  <label>'.$langs->trans("Town").'</label>
-                  <input type="text" class="form-control" value="'.$mythirdpartyaccount->town.'" name="town">
-                </div>
-                <div class="form-group">
-                  <label>'.$langs->trans("Zip").'</label>
-                  <input type="text" class="form-control input-small" value="'.$mythirdpartyaccount->zip.'" name="zip">
-                </div>
-                <div class="form-group">
-                  <label>'.$langs->trans("State").'</label>
-                  <input type="text" class="form-control" placeholder="'.$langs->trans("StateOrCounty").'" name="stateorcounty" value="">
-                </div>
-                <div class="form-group">
-                  <label>'.$langs->trans("Country").'</label><br>';
-                $countryselected = (GETPOSTISSET('country_id')?GETPOST('country_id','aZ09'):$mythirdpartyaccount->country_id);
-                $exclude_country_code = array();
-                if (! empty($conf->global->SELLYOURSAAS_EXCLUDE_COUNTRY_CODES)) $exclude_country_code = explode(',', $conf->global->SELLYOURSAAS_EXCLUDE_COUNTRY_CODES);
-                print '<input type="hidden" name="country_id_old" value="'.$countryselected.'">'."\n";
-                print $form->select_country($countryselected, 'country_id', '', 0, 'minwidth300', 'code2', 0, 1, 0, $exclude_country_code);
-                print '
-                                </div>
-                                <div class="form-group">
-                                  <label>'.$langs->trans("VATIntra").'</label> ';
-                if (! empty($mythirdpartyaccount->tva_assuj) && empty($mythirdpartyaccount->tva_intra))
-                {
-                    print img_warning($langs->trans("Mandatory"), 'class="hideifnonassuj"');
-                }
+	                <div class="form-group">
+	                  <label>'.$langs->trans("AddressLine").'</label>
+	                  <input type="text" class="form-control" placeholder="'.$langs->trans("HouseNumberAndStreet").'" value="'.$mythirdpartyaccount->address.'" name="address">
+	                </div>
+	                <div class="form-group">
+	                  <label>'.$langs->trans("Town").'</label>
+	                  <input type="text" class="form-control" value="'.$mythirdpartyaccount->town.'" name="town">
+	                </div>
+	                <div class="form-group">
+	                  <label>'.$langs->trans("Zip").'</label>
+	                  <input type="text" class="form-control input-small" value="'.$mythirdpartyaccount->zip.'" name="zip">
+	                </div>
+	                <div class="form-group">
+	                  <label>'.$langs->trans("State").'</label>
+	                  <input type="text" class="form-control" placeholder="'.$langs->trans("StateOrCounty").'" name="stateorcounty" value="">
+	                </div>
+	                <div class="form-group">
+	                  <label>'.$langs->trans("Country").'</label><br>';
+	                $countryselected = (GETPOSTISSET('country_id')?GETPOST('country_id','aZ09'):$mythirdpartyaccount->country_id);
+	                $exclude_country_code = array();
+	                if (! empty($conf->global->SELLYOURSAAS_EXCLUDE_COUNTRY_CODES)) $exclude_country_code = explode(',', $conf->global->SELLYOURSAAS_EXCLUDE_COUNTRY_CODES);
+	                print '<input type="hidden" name="country_id_old" value="'.$countryselected.'">'."\n";
+	                print $form->select_country($countryselected, 'country_id', '', 0, 'minwidth300', 'code2', 0, 1, 0, $exclude_country_code);
+	                print '
+	                                </div>
+	                                <div class="form-group">
+	                                  <label>'.$langs->trans("VATIntra").'</label> ';
+	                if (! empty($mythirdpartyaccount->tva_assuj) && empty($mythirdpartyaccount->tva_intra))
+	                {
+	                    print img_warning($langs->trans("Mandatory"), 'class="hideifnonassuj"');
+	                }
 
-                $placeholderforvat='';
-                if ($mythirdpartyaccount->country_code == 'FR') $placeholderforvat='Exemple: FR12345678';
-                elseif ($mythirdpartyaccount->country_code == 'BE') $placeholderforvat='Exemple: BE12345678';
-                elseif ($mythirdpartyaccount->country_code == 'ES') $placeholderforvat='Exemple: ES12345678';
-                else $placeholderforvat=$langs->trans("EnterVATHere");
+	                $placeholderforvat='';
+	                if ($mythirdpartyaccount->country_code == 'FR') $placeholderforvat='Exemple: FR12345678';
+	                elseif ($mythirdpartyaccount->country_code == 'BE') $placeholderforvat='Exemple: BE12345678';
+	                elseif ($mythirdpartyaccount->country_code == 'ES') $placeholderforvat='Exemple: ES12345678';
+	                else $placeholderforvat=$langs->trans("EnterVATHere");
 
-                print '
-					<br>
-                  <input type="hidden" name="vatassuj_old" value="'.($mythirdpartyaccount->tva_assuj).'">
-                  <input type="checkbox" style="vertical-align: top" class="inline-block"'.($mythirdpartyaccount->tva_assuj?' checked="checked"':'').'" id="vatassuj" name="vatassuj"> '.$langs->trans("VATIsUsed").'
-					<br>
-                  <input type="hidden" name="vatnumber_old" value="'.$mythirdpartyaccount->tva_intra.'">
-                  <input type="text" class="input-small quatrevingtpercent hideifnonassuj" value="'.$mythirdpartyaccount->tva_intra.'" name="vatnumber" placeholder="'.$placeholderforvat.'">
-                    ';
-                if (empty($conf->global->MAIN_DISABLEVATCHECK) && $mythirdpartyaccount->isInEEC() && (GETPOST('admin','alpha')))
-                {
-                    if (! empty($conf->use_javascript_ajax))
-                    {
-                        print "\n";
-                        print '<script language="JavaScript" type="text/javascript">';
-                        print "function CheckVAT(a) {\n";
-                        print "newpopup('".DOL_URL_ROOT."/societe/checkvat/checkVatPopup.php?vatNumber='+a,'".dol_escape_js($langs->trans("VATIntraCheckableOnEUSite"))."', 540, 350);\n";
-                        print "}\n";
-                        print '</script>';
-                        print "\n";
-                        $s.='<a href="#" class="hideonsmartphone" onclick="javascript: CheckVAT(document.formsoc.vatnumber.value);">'.$langs->trans("VATIntraCheck").'</a>';
-                        $s = $form->textwithpicto($s, $langs->trans("VATIntraCheckDesc", $langs->transnoentitiesnoconv("VATIntraCheck")), 1);
-                    }
-                    else
-                    {
-                        $s.='<a href="'.$langs->transcountry("VATIntraCheckURL", $mythirdpartyaccount->country_id).'" target="_blank">'.img_picto($langs->trans("VATIntraCheckableOnEUSite"), 'help').'</a>';
-                    }
-                    print $s;
-                }
-                print '
-                </div>
-              </div>
-              <!-- END FORM BODY -->
+	                print '
+						<br>
+	                  <input type="hidden" name="vatassuj_old" value="'.($mythirdpartyaccount->tva_assuj).'">
+	                  <input type="checkbox" style="vertical-align: top" class="inline-block"'.($mythirdpartyaccount->tva_assuj?' checked="checked"':'').'" id="vatassuj" name="vatassuj"> '.$langs->trans("VATIsUsed").'
+						<br>
+	                  <input type="hidden" name="vatnumber_old" value="'.$mythirdpartyaccount->tva_intra.'">
+	                  <input type="text" class="input-small quatrevingtpercent hideifnonassuj" value="'.$mythirdpartyaccount->tva_intra.'" name="vatnumber" placeholder="'.$placeholderforvat.'">
+	                    ';
+	                if (empty($conf->global->MAIN_DISABLEVATCHECK) && $mythirdpartyaccount->isInEEC() && (GETPOST('admin','alpha')))
+	                {
+	                    if (! empty($conf->use_javascript_ajax))
+	                    {
+	                        print "\n";
+	                        print '<script language="JavaScript" type="text/javascript">';
+	                        print "function CheckVAT(a) {\n";
+	                        print "newpopup('".DOL_URL_ROOT."/societe/checkvat/checkVatPopup.php?vatNumber='+a,'".dol_escape_js($langs->trans("VATIntraCheckableOnEUSite"))."', 540, 350);\n";
+	                        print "}\n";
+	                        print '</script>';
+	                        print "\n";
+	                        $s.='<a href="#" class="hideonsmartphone" onclick="javascript: CheckVAT(document.formsoc.vatnumber.value);">'.$langs->trans("VATIntraCheck").'</a>';
+	                        $s = $form->textwithpicto($s, $langs->trans("VATIntraCheckDesc", $langs->transnoentitiesnoconv("VATIntraCheck")), 1);
+	                    }
+	                    else
+	                    {
+	                        $s.='<a href="'.$langs->transcountry("VATIntraCheckURL", $mythirdpartyaccount->country_id).'" target="_blank">'.img_picto($langs->trans("VATIntraCheckableOnEUSite"), 'help').'</a>';
+	                    }
+	                    print $s;
+	                }
+	                print '
+	                </div>
+	              </div>
+	              <!-- END FORM BODY -->
 
-              <div>
-                <input type="submit" name="submit" value="'.$langs->trans("Save").'" class="btn green-haze btn-circle">
-              </div>
+	              <div>
+	                <input type="submit" name="submit" value="'.$langs->trans("Save").'" class="btn green-haze btn-circle">
+	              </div>
 
-            </form>
-            <!-- END FORM DIV -->
-          </div> <!-- END PORTLET-BODY -->
-        </div>
+	            </form>
+	            <!-- END FORM DIV -->
+	           </div> <!-- END PORTLET-BODY -->
+            </div>
+';
 
-	    </div>
+	                if (! GETPOST('deleteaccount')) {
+	                	print '<div class="center"><br>';
+	                	$urltoenterpaymentmode = $_SERVER["PHP_SELF"].'?mode=registerpaymentmode&backtourl='.urlencode($_SERVER["PHP_SELF"].'?mode='.$mode);
+	                	print '<a href="'.$urltoenterpaymentmode.'" class=""><span class="fa fa-credit-card paddingright"></span>';
+	                	if ($nbpaymentmodeok) print $langs->trans("ModifyPaymentMode").'...';
+	                	else print $langs->trans("AddAPaymentMode").'...';
+	                	print '</a>';
+	                	print '<br><br><br>';
+	                	print '</div>';
+	                }
+
+print '
+
+	      </div>
 		';
 
 print '<script type="text/javascript" language="javascript">
@@ -209,7 +223,7 @@ print '
 	                  </div>
 	                </div>
 	                <div class="form-group">
-	                  <label>'.$langs->trans("OptinForCommercialMessages").'</label><br>
+	                  <!--<label>'.$langs->trans("OptinForCommercialMessages").'</label><br>-->
 	                  <input type="checkbox" class="form-control inline valignmiddle" style="margin-top: 0" value="1" '.($mythirdpartyaccount->array_options['options_optinmessages'] ? ' checked' : '').' name="optinmessages">
 						<span class="inline valignmiddle opacitymedium">'.$langs->trans("OptinForCommercialMessagesOnMyAccount", $sellyoursaasname).'</span>
 	                </div>
@@ -257,20 +271,9 @@ print '
 
 			';
 
-
             if (! GETPOST('deleteaccount')) {
-                print '<div class="center"><br>';
-                $urltoenterpaymentmode = $_SERVER["PHP_SELF"].'?mode=registerpaymentmode&backtourl='.urlencode($_SERVER["PHP_SELF"].'?mode='.$mode);
-                print '<a href="'.$urltoenterpaymentmode.'" class="">';
-                if ($nbpaymentmodeok) print $langs->trans("ModifyPaymentMode").'...';
-                else print $langs->trans("AddAPaymentMode").'...';
-                print '</a>';
-                print '<br>';
-                print '</div>';
+              	print '<div class="center"><br><a href="#deletemyaccountarea" class="deletemyaccountclick"><span class="fa fa-trash paddingright"></span>'.$langs->trans("DeleteMyAccount").'...</a><br><br><br></div>';
             }
-
-
-            if (! GETPOST('deleteaccount')) print '<div class="center"><br><a href="#deletemyaccountarea" class="deletemyaccountclick">'.$langs->trans("DeleteMyAccount").'...<br><br></a></div>';
 
             print '
 			<script type="text/javascript" language="javascript">
