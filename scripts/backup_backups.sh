aa637a84274e4afb6218f9abe7ef258d9e357d44
Laurent Destailleur <eldy@destailleur.fr>
2021-06-28 14:42:01 +0200

    Fix dir

--- scripts/backup_backups.sh
+++ scripts/backup_backups.sh
@@ -173,6 +173,9 @@ if [[ "x$instanceserver" == "x1" ]]; then
 	echo
 	echo `date +%Y%m%d%H%M%S`" Do rsync of customer directories $DIRSOURCE2/osu to $SERVDESTI..."
 
+	echo `date +%Y%m%d%H%M%S`" Do rsync of emptydir to $SERVDESTICURSOR:$DIRDESTI2/backupold_$HISTODIR/..."
+	rsync $TESTN --delete -a $HOME/emptydir/ $USER@$SERVDESTICURSOR:$DIRDESTI2/backupold_$HISTODIR/
+
 	for i in 'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z' '0' '1' '2' '3' '4' '5' '6' '7' '8' '9' ; do
 		echo `date +%Y%m%d%H%M%S`" Process directory $backupdir/osu$i"
 		nbofdir=`ls -d $backupdir/osu$i* | wc -l`
