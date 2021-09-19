2fe57f9882eb4cd802b111389885d22401889846
Laurent Destailleur <eldy@destailleur.fr>
2021-09-19 15:16:13 +0200

    Enhance log into apache watchdog3

--- scripts/apache_watchdog_daemon3.sh
+++ scripts/apache_watchdog_daemon3.sh
@@ -52,17 +52,19 @@ while read ; do
     sleep 5
 	export now=`date '+%Y-%m-%d %H:%M:%S'`
 	echo "$now ----- Found a segfault, now kicking apache..." >> /var/log/apache_watchdog3.log 2>&1
-    sleep 5
+    sleep 2
 	/etc/init.d/apache2 stop >> /var/log/apache_watchdog3.log 2>&1
 	sleep 5
 	killall -9 apache2 >> /var/log/apache_watchdog3.log 2>&1
-	sleep 5
+	sleep 10
 	export now=`date '+%Y-%m-%d %H:%M:%S'`
 	echo "$now Now restart apache..." >> /var/log/apache_watchdog3.log 2>&1
 	/etc/init.d/apache2 start >> /var/log/apache_watchdog3.log 2>&1
-	
-	echo "Apache seg fault detected by apache_watchdog_daemon3. Apache was killed and started." | mail -aFrom:$EMAILFROM -s "[Warning] Apache seg fault detected on "`hostname`". Apache was killed and started." $EMAILTO
-	sleep 5
+
+	sleep 2
+	echo "Apache seg fault detected by apache_watchdog_daemon3. Apache was killed and started." | mail -aFrom:$EMAILFROM -s "[Warning] Apache seg fault detected on "`hostname`". Apache was killed and started at $now." $EMAILTO
+
+	sleep 2
 	export now=`date '+%Y%m%d%H%M%S'`
 	mv /var/crash/_usr_sbin_apache2.0.crash /var/crash/_usr_sbin_apache2.0.crash."$now" >> /var/log/apache_watchdog3.log 2>&1
 #fi
