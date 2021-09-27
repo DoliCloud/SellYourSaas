3783f5e6fbcd1e7c22c7c3f421e472359198e74a
Laurent Destailleur <eldy@destailleur.fr>
2021-09-27 22:22:09 +0200

    Fix link

--- backoffice/instance_backup.php
+++ backoffice/instance_backup.php
@@ -167,7 +167,7 @@ if ($ispaid) {
 $tmparray = explode('.', $object->ref_customer);
 
 $moveinstancestringtoshow .= "su - admin"."\n";
-$moveinstancestringtoshow .= "chmod a+r /etc/apache2/".$object->ref_customer.".key\n";
+$moveinstancestringtoshow .= "chmod a+r /etc/apache2/".getDomainFromURL($object->ref_customer, 2).".key\n";
 $moveinstancestringtoshow .= $conf->global->DOLICLOUD_SCRIPTS_PATH.'/move_instance.php '.$object->ref_customer.' '.$tmparray[0].'.withNEW.'.getDomainFromURL($object->ref_customer, 1).' (test|confirm)';
 
 
