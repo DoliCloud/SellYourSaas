0024688622024e25f44a6c12d78734b11e26f224
Laurent Destailleur <eldy@destailleur.fr>
2021-12-06 20:19:38 +0100

    Set focus on first field

--- myaccount/tpl/instances.tpl.php
+++ myaccount/tpl/instances.tpl.php
@@ -913,7 +913,7 @@ if ($MAXINSTANCES && count($listofcontractid) < $MAXINSTANCES) {
 
         			<div class="horizontal-fld clearboth margintoponly">
         			<div class="control-group required">
-        			<label class="control-label" for="password" trans="1">'.$langs->trans("Password").'</label><input name="password" type="password" maxlength="128" required />
+        			<label class="control-label" for="password" trans="1">'.$langs->trans("Password").'</label><input name="password" type="password" maxlength="128"'.(GETPOST('addanotherinstance', 'int') ? ' autofocus' : '').' required />
         			</div>
         			</div>
         			<div class="horizontal-fld margintoponly">
