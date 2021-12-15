218448eca89c1a7868fbdfffc1649114d683fe2a
Laurent Destailleur <eldy@destailleur.fr>
2021-12-15 16:18:02 +0100

    Fix sql

--- myaccount/register_instance.php
+++ myaccount/register_instance.php
@@ -642,7 +642,7 @@ if ($reusecontractid) {
 	$nbofinstanceindeployment=-1;
 	$select = 'SELECT COUNT(*) as nb FROM '.MAIN_DB_PREFIX."contrat_extrafields";
 	$select .= " WHERE options_deployment_host = '".$db->escape($serverdeployement)."'";
-	$select .= " WHERE deployment_status IN ('processing')";
+	$select .= " AND deployment_status IN ('processing')";
 	$resselect = $db->query($select);
 	if ($resselect) {
 		$objselect = $db->fetch_object($resselect);
