8cb387ff6ec9bb2e5d18bc57cf96936789742431
Laurent Destailleur <eldy@destailleur.fr>
2022-01-28 03:21:05 +0100

    Fix apparmor, fix doc

--- scripts/desktop_apt_upgrade.sh
+++ scripts/desktop_apt_upgrade.sh
@@ -9,10 +9,10 @@
 source /etc/lsb-release
 
 if [ "x$2" == "x" ]; then
-   echo "Usage:   $0  hostfile  [hostgrouporname]"
+   echo "Usage:   $0  hostfile  [hostgrouporname]  (reboot)"
    echo "         [hostgrouporname] can be 'master', 'deployment', 'web', 'remotebackup', or list separated with comma like 'master,deployment' (default)"
    echo "Example: $0  myhostfile  master,deployment"
-   echo "Example: $0  myhostfile  withXmysellyoursaasdomain.com"
+   echo "Example: $0  myhostfile  withXmysellyoursaasdomain.com  reboot"
    exit 1
 fi
 
@@ -20,6 +20,8 @@ target=$2
 if [ "x$target" == "x" ]; then
 	target="master"
 fi
+
+reboot=$3
 
 export currentpath=$(dirname "$0")
 
@@ -30,7 +32,12 @@ pwd
 
 
 #command="ansible-playbook -K launch_apt_upgrade.yml -i hosts-$1 -e 'target="$target"' --limit=*.mydomain.com"
-command='ansible-playbook -K launch_apt_upgrade.yml -i hosts-'$1' -e "target='$target' command='confirm'"'
+
+if [ "x$reboot" == "x" ]; then
+	command='ansible-playbook -K launch_apt_upgrade.yml -i hosts-'$1' -e "target='$target'"'
+else
+	command='ansible-playbook -K launch_apt_upgrade.yml -i hosts-'$1' -e "target='$target' reboot='1'"'
+fi
 echo "$command"
 eval $command
 
