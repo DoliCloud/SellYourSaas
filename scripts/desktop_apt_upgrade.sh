715bd3ba586883b5fe38fafab4b88e5ed616915a
Laurent Destailleur <eldy@destailleur.fr>
2022-01-28 01:05:05 +0100

    Add script for apt update and upgrade

--- scripts/desktop_apt_upgrade.sh
+++ scripts/desktop_apt_upgrade.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#---------------------------------------------------------
-# Script to run remotely the clean.sh
-#
-# /pathto/clean_remote.sh hostfile [hostgrouporname]
-#---------------------------------------------------------
+#----------------------------------------------------------------
+# This script allows to launch an apt update / upgrade on remote servers
+#----------------------------------------------------------------
+
 
 #set -e
 
@@ -13,13 +12,13 @@ if [ "x$2" == "x" ]; then
    echo "Usage:   $0  hostfile  [hostgrouporname]"
    echo "         [hostgrouporname] can be 'master', 'deployment', 'web', 'remotebackup', or list separated with comma like 'master,deployment' (default)"
    echo "Example: $0  myhostfile  master,deployment"
-   echo "Example: $0  myhostfile  withX.mysellyoursaasdomain.com"
+   echo "Example: $0  myhostfile  withXmysellyoursaasdomain.com"
    exit 1
 fi
 
 target=$2
 if [ "x$target" == "x" ]; then
-	target="master,deployment"
+	target="master"
 fi
 
 export currentpath=$(dirname "$0")
@@ -30,9 +29,11 @@ echo "Execute ansible for host group $1 and targets $2"
 pwd
 
 
-#command="ansible-playbook -K launch_clean.yml -i hosts-$1 -e 'target="$target"' --limit=*.mydomain.com"
-command='ansible-playbook -K launch_clean.yml -i hosts-'$1' -e "target='$target' command='confirm'"'
+#command="ansible-playbook -K launch_apt_upgrade.yml -i hosts-$1 -e 'target="$target"' --limit=*.mydomain.com"
+command='ansible-playbook -K launch_apt_upgrade.yml -i hosts-'$1' -e "target='$target' command='confirm'"'
 echo "$command"
 eval $command
 
 echo "Finished."
+
+
