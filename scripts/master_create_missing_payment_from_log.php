439e08264f5f986d91c29f58d0efd66678bc82f6
Laurent Destailleur <eldy@destailleur.fr>
2022-02-28 16:42:21 +0100

    Doc

--- scripts/master_create_missing_payment_from_log.php
+++ scripts/master_create_missing_payment_from_log.php
@@ -1,1 +1,5 @@
 -- TODO script to create missing payment after successful Stripe payement but no recording into database.
+
+
+Scan "Successfuly charge card pm_.... for invoice ...."
+and create payement for thos invoices.
