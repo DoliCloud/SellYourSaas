10f23191ebefd6465b6eb0abded0e5b2f97d01e6
Laurent Destailleur <eldy@destailleur.fr>
2022-06-28 00:03:13 +0200

    Add option "all" into install check script.

--- scripts/desktop_install_check.sh
+++ scripts/desktop_install_check.sh
@@ -9,7 +9,7 @@
 source /etc/lsb-release
 
 if [ "x$1" == "x" ]; then
-   echo "Usage:   $0  hostfile  [hostgrouporname]  (php74)"
+   echo "Usage:   $0  hostfile  [hostgrouporname]  (apache|php74|all)"
    echo "         [hostgrouporname] can be 'master', 'deployment', 'web', 'remotebackup', or list separated with comma like 'master,deployment' (default)"
    echo "Example: $0  myhostfile  master,deployment"
    echo "Example: $0  myhostfile  withX.mysellyoursaasdomain.com"
@@ -21,8 +21,14 @@ if [ "x$target" == "x" ]; then
 	target="master"
 fi
 
+if [ "x$3" == "xall" ]; then
+	all=1
+fi
 if [ "x$3" == "xphp74" ]; then
 	php74=1
+fi
+if [ "x$3" == "xapache" ]; then
+	apache=1
 fi
 
 
@@ -36,11 +42,16 @@ pwd
 
 #command="ansible-playbook -K launch_install_check.yml -i hosts-$1 -e 'target="$target"' --limit=*.mydomain.com"
 
-if [ "x$php74" == "x" ]; then
-	command='ansible-playbook -K launch_install_check.yml -i hosts-'$1' -e "target='$target'"'
-else
+if [ "x$all" == "x1" ]; then
+	command='ansible-playbook -K launch_install_check.yml -i hosts-'$1' -e "target='$target' apache='1' php74='1'"'
+elif [ "x$php74" == "x1" ]; then
 	command='ansible-playbook -K launch_install_check.yml -i hosts-'$1' -e "target='$target' php74='1'"'
+elif [ "x$apache" == "x1" ]; then
+	command='ansible-playbook -K launch_install_check.yml -i hosts-'$1' -e "target='$target' apache='1'"'
+else
+	command='ansible-playbook -K launch_install_check.yml -i hosts-'$1' -e "target='$target'"'
 fi
+
 echo "$command"
 eval $command
 
