85d3f9b3d9694dc7d10e1e90ba9c23241947fab1
Laurent Destailleur <eldy@destailleur.fr>
2022-07-13 04:40:52 +0200

    wip

--- scripts/smtp_watchdog_daemon1.sh
+++ scripts/smtp_watchdog_daemon1.sh
@@ -71,8 +71,9 @@ while read -r line ; do
 				if [ "x$processid" != "x" ]; then
 					# And now try to find the username of process id
 					echo "Try to find username from processid=$processid"
-					export command="ps fauxwwZ | grep $processid"
-					export usernamestring=`$command`
+					export command="ps fauxwZ | grep $processid"
+					echo "Execute command $command"
+					usernamestring=`$command`
 				fi				
 			fi
 		fi
