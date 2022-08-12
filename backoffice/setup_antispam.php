5b9ad1eecf984f4e78ed45a483bb3fab427b9720
Laurent Destailleur <eldy@destailleur.fr>
2022-08-12 13:29:10 +0200

    Clean setup page

--- backoffice/setup_antispam.php
+++ backoffice/setup_antispam.php
@@ -115,54 +115,6 @@ print '<td>'.$langs->trans('Options').'</td>';
 print '<td></td>';
 print '</tr>';
 
-//$dirforspam = DOL_DATA_ROOT.'/sellyoursaas_local/spam';
-$dirforspam = '/tmp/spam';
-
-print '<tr class="oddeven nohover">';
-print '<td>';
-$filemaster = $dirforspam.'/blacklistfrom';
-$file = $filemaster;
-$htmltext = $langs->trans("ExampleContentOfFileOnMaster", $file).'<br>';
-if (dol_is_file($filemaster)) {
-	$htmltext .= dol_htmlentitiesbr(file_get_contents($filemaster));
-}
-print $form->textwithpicto($langs->trans("FileFor", 'blacklistfrom').' <span class="opacitymedium">('.$langs->trans("FileToEditedManually").')</span>', '', 1, 'help', '', 0, 3, 'blacklistfrom');
-print '</td>';
-print '<td>';
-print $form->textwithpicto($file, $htmltext, 1, 'help', '', 0, 3, 'blacklistfrom');
-print '</td>';
-print '</tr>';
-
-print '<tr class="oddeven nohover">';
-print '<td>';
-$filemaster = $dirforspam.'/blacklistcontent';
-$file = $filemaster;
-$htmltext = $langs->trans("ExampleContentOfFileOnMaster", $file).'<br>';
-if (dol_is_file($filemaster)) {
-	$htmltext .= dol_htmlentitiesbr(file_get_contents($filemaster));
-}
-print $form->textwithpicto($langs->trans("FileFor", 'blacklistcontent').' <span class="opacitymedium">('.$langs->trans("FileToEditedManually").')</span>', '', 1, 'help', '', 0, 3, 'blacklistcontent');
-print '</td>';
-print '<td>';
-print $form->textwithpicto($file, $htmltext, 1, 'help', '', 0, 3, 'blacklistcontent');
-print '</td>';
-print '</tr>';
-
-print '<tr class="oddeven nohover">';
-print '<td>';
-$filemaster = $dirforspam.'/blacklistip';
-$file = $filemaster;
-$htmltext = $langs->trans("ExampleContentOfFileOnMaster", $file).'<br>';
-if (dol_is_file($filemaster)) {
-	$htmltext .= dol_htmlentitiesbr(file_get_contents($filemaster));
-}
-print $form->textwithpicto($langs->trans("FileFor", 'blacklistip'), $langs->trans("FileEditedAutomaticallyByMailWrapperOnAbuseDetection"), 1, 'help', '', 0, 3, 'blacklistip');
-print '</td>';
-print '<td>';
-print $form->textwithpicto($file, $htmltext, 1, 'help', '', 0, 3, 'blacklistip');
-print '</td>';
-print '</tr>';
-
 print '<tr class="oddeven nohover">';
 print '<td>';
 print $langs->trans("EnableAlertEmailOnWebhookSpamReport");
