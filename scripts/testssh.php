1512c3bc2547169ef8d73932a16c195f85e67afa
Laurent Destailleur <eldy@destailleur.fr>
2022-10-07 15:41:48 +0200

    Doc

--- scripts/testssh.php
+++ scripts/testssh.php
@@ -4,6 +4,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // ssh osuu3y383ke4@testdeploy2apollon5.with5.dolicloud.com
-$connection = ssh2_connect('testdeploy2apollon5.with5.dolicloud.com', 22, array('hostkey' => 'ecdsa-sha2-nistp256,ssh-rsa'));
+// hostkey can be:
+// - ssh-rsa (old)
+// - ecdsa-sha2-nistp256
+$connection = ssh2_connect('testdeploy2apollon5.with5.dolicloud.com', 22, array('hostkey' => 'ecdsa-sha2-nistp256'));
 
 var_dump($connection);
