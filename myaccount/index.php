74d99d9b8813446ff6a90b3df7fbba30c5622cb1
Laurent Destailleur <eldy@destailleur.fr>
2022-10-21 15:23:40 +0200

    Clean code

--- myaccount/index.php
+++ myaccount/index.php
@@ -3550,15 +3550,11 @@ print '
 	</div>
 
 
-	<!-- Bootstrap core JavaScript
+	<!-- Bootstrap core JavaScript for menu popup
 	================================================== -->
 	<!-- Placed at the end of the document so the pages load faster -->
 	<script src="dist/js/popper.min.js"></script>
 	<script src="dist/js/bootstrap.min.js"></script>
-	<!--
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
-	-->
 ';
 
 
