4ff8c7019ca49dcec0f18887567155a28ca3df57
lmarcouiller <lucas.marcouiller@epsi.fr>
2022-12-21 11:59:43 +0100

    update wrong traduction

--- core/modules/modSellYourSaas.class.php
+++ core/modules/modSellYourSaas.class.php
@@ -779,7 +779,7 @@ class modSellYourSaas extends DolibarrModules
 			'1'=>'Yes'
 		);
 		$param=array('options'=>$arrayoptions);
-		$resultx=$extrafields->addExtraField('checkbosnonprofitorga',       "NonProfitOrganisation",   'select', 199,   '3', 'thirdparty', 0, 0,'0',    $param, 1, '', -1, 0, '', '', 'sellyoursaas@sellyoursaas', '$conf->sellyoursaas->enabled');
+		$resultx=$extrafields->addExtraField('checkbosnonprofitorga',       "NonProfitOrganization",   'select', 199,   '3', 'thirdparty', 0, 0,'0',    $param, 1, '', -1, 0, '', '', 'sellyoursaas@sellyoursaas', '$conf->sellyoursaas->enabled');
 		//$resultx=$extrafields->addExtraField('separatorthirdpartyend',                      "Other", 'separate',199,    '', 'thirdparty', 0, 0, '',     '', 1, '',  1, 0, '', '', 'sellyoursaas@sellyoursaas', '$conf->sellyoursaas->enabled');
 
 		// Contract
