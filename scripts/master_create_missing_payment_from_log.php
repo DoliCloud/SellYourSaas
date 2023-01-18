980210af332bb712f43456738cabab639d5e2930
Laurent Destailleur <eldy@destailleur.fr>
2023-01-18 01:10:52 +0100

    Prepare script

--- scripts/master_create_missing_payment_from_log.php
+++ scripts/master_create_missing_payment_from_log.php
@@ -1,5 +1,51 @@
--- TODO script to create missing payment after successful Stripe payment but no recording into database.
+#!/usr/bin/php
+<?php
+/* Copyright (C) 2020-2021 Laurent Destailleur	<eldy@users.sourceforge.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * or see http://www.gnu.org/
+ *
+ * Migrate an old instance on a new server.
+ * Script must be ran with admin from master server.
+ */
+
+if (!defined('NOSESSION')) define('NOSESSION', '1');
+
+$sapi_type = php_sapi_name();
+$script_file = basename(__FILE__);
+$path=dirname(__FILE__).'/';
+
+// Test if batch mode
+if (substr($sapi_type, 0, 3) == 'cgi') {
+	echo "Error: You are using PHP for CGI. To execute ".$script_file." from command line, you must use PHP for CLI mode.\n";
+	exit(-1);
+}
+
+// Global variables
+$version='1.0';
+$error=0;
+
+// Include Dolibarr environment
+@set_time_limit(0);							// No timeout for this script
+define('EVEN_IF_ONLY_LOGIN_ALLOWED', 1);		// Set this define to 0 if you want to lock your script when dolibarr setup is "locked to admin user only".
+//if (! defined('NOREQUIREDB'))              define('NOREQUIREDB', '1');                                  // Do not create database handler $db
+//if (! defined('NOREQUIREUSER'))            define('NOREQUIREUSER', '1');                                // Do not load object $user
 
 
+print 'Script to create missing payment after successful Stripe payment but no recording into database.';
+
+/*
 Scan "Successfuly charge card pm_.... for invoice ...."
 and create payment for those invoices.
+*/
