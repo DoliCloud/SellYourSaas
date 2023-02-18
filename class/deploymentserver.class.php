98b2d7542fee92ef8acbae081a55de8d7560dee9
Laurent Destailleur <eldy@destailleur.fr>
2023-02-18 20:47:45 +0100

    compatibility v18

--- class/deploymentserver.class.php
+++ class/deploymentserver.class.php
@@ -789,9 +789,10 @@ class Deploymentserver extends CommonObject
 	 *	Return a thumb for kanban views
 	 *
 	 *	@param      string	    $option                 Where point the link (0=> main card, 1,2 => shipment, 'nolink'=>No link)
+	 *  @param		array		$arraydata				Array of data
 	 *  @return		string								HTML Code for Kanban thumb.
 	 */
-	public function getKanbanView($option = '')
+	public function getKanbanView($option = '', $arraydata = null)
 	{
 		 global $langs;
 
