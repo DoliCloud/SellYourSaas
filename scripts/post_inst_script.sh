2af638df94468343e307693d42ac4e2da84c2ed9
Laurent Destailleur <eldy@destailleur.fr>
2023-02-26 10:58:10 +0100

    Clean code

--- scripts/post_inst_script.sh
+++ scripts/post_inst_script.sh
@@ -6,26 +6,10 @@
 #
 
 export now=`date +'%Y-%m-%d %H:%M:%S'`
-
-echo
-#echo "####################################### ${0} ${1}"
-#echo "${0} ${@}"
-#echo "# user id --------> $(id -u)"
-#echo "# now ------------> $now"
-#echo "# PID ------------> ${$}"
-#echo "# PWD ------------> $PWD" 
-#echo "# arguments ------> ${@}"
-#echo "# path to me -----> ${0}"
-#echo "# parent path ----> ${0%/*}"
-#echo "# my name --------> ${0##*/}"
-#echo "# realname -------> $(realpath ${0})"
-#echo "# realname name --> $(basename $(realpath ${0}))"
-#echo "# realname dir ---> $(dirname $(realpath ${0}))"
-
 export PID=${$}
 export scriptdir=$(dirname $(realpath ${0}))
 
-
+echo
 echo "***** $0 *****"
 
 if [ "$(id -u)" != "0" ]; then
