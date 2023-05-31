80a75c4bb3f33bdbe060437e3ba85f2d31f8f686
lmarcouiller <lucas.marcouiller@epsi.fr>
2023-05-31 15:27:07 +0200

    add verification on payment method

--- myaccount/tpl/action_create_recinvoice_after_payment_creation.tpl.php
+++ myaccount/tpl/action_create_recinvoice_after_payment_creation.tpl.php
@@ -413,7 +413,7 @@ if (! $error) {
 				}
 			}
 		}
-		if (! $error) {
+		if (! $error && !$thirdpartyhadalreadyapaymentmode) {
 			$comment = 'Services for '.$contract->ref.' after the creation of a first payment method';
 			$sellyoursaasutils = new SellYourSaasUtils($db);
 			$result = $sellyoursaasutils->sellyoursaasRemoteAction('actionafterpaid', $contract, 'admin', '', '', 0, $comment);
