1a6511de74c96b8ccc33c7413841974c008db7ac
Laurent Destailleur <eldy@destailleur.fr>
2024-02-14 00:42:54 +0100

    Log

--- scripts/master_move_instance.php
+++ scripts/master_move_instance.php
@@ -951,7 +951,7 @@ if ($mode != 'confirmredirect' && $mode != 'confirmmaintenance') {
 		$suspendmessage = 'https://'.$newinstance;
 		$newip = $newobject->array_options['options_deployment_host'];
 		$comment = 'Move instance keeping a redirect to '.$suspendmessage.', we also set the new IP '.$newip.' into the old DNS file.';
-		print '--- Switch old instance in redirect maintenance mode (redirect to '.$suspendmessage.', new ip to '.$newip.")\n";
+		print '--- Switch old instance in redirect maintenance mode by calling remote action suspendredirect on the old server (set redirect to '.$suspendmessage.', and set the new ip to '.$newip." in DNS)\n";
 
 		$result = $sellyoursaasutils->sellyoursaasRemoteAction('suspendredirect', $oldobject, 'admin', '', '', '0', $comment, 300, $newip);
 		if ($result <= 0) {
