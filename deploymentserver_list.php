29175c470c76ba03db5ec30804f6badf8fcceebb
Laurent Destailleur <eldy@destailleur.fr>
2024-03-12 03:42:26 +0100

    Fix duplicate tooltip on ref

--- deploymentserver_list.php
+++ deploymentserver_list.php
@@ -815,7 +815,7 @@ while ($i < $imaxinloop) {
 			}
 
 			if (!empty($arrayfields['t.'.$key]['checked'])) {
-				print '<td'.($cssforfield ? ' class="'.$cssforfield.(preg_match('/tdoverflow/', $cssforfield) ? ' classfortooltip' : '').'"' : '');
+				print '<td'.($cssforfield ? ' class="'.$cssforfield.((preg_match('/tdoverflow/', $cssforfield) && !in_array($val['type'], array('ref', 'ip', 'url')) && !is_numeric($object->$key)) ? ' classfortooltip' : '').'"' : '');
 				if (preg_match('/tdoverflow/', $cssforfield) && !in_array($val['type'], array('ref', 'ip', 'url')) && !is_numeric($object->$key)) {
 					print ' title="'.dol_escape_htmltag($object->$key).'"';
 				}
