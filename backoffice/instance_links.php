58af553edd3197a14cb68e6ed15665c8bd0df359
Laurent Destailleur <eldy@destailleur.fr>
2024-08-03 18:52:14 +0200

    Better path

--- backoffice/instance_links.php
+++ backoffice/instance_links.php
@@ -59,9 +59,9 @@ require_once DOL_DOCUMENT_ROOT."/core/lib/company.lib.php";
 require_once DOL_DOCUMENT_ROOT."/core/lib/date.lib.php";
 require_once DOL_DOCUMENT_ROOT."/core/lib/security2.lib.php";
 require_once DOL_DOCUMENT_ROOT."/core/class/html.formcompany.class.php";
-dol_include_once("/sellyoursaas/core/lib/sellyoursaas.lib.php");
-dol_include_once("/sellyoursaas/class/sellyoursaascontract.class.php");
-dol_include_once("/sellyoursaas/class/sellyoursaasutils.class.php");
+dol_include_once("sellyoursaas/core/lib/sellyoursaas.lib.php");
+dol_include_once("sellyoursaas/class/sellyoursaascontract.class.php");
+dol_include_once("sellyoursaas/class/sellyoursaasutils.class.php");
 dol_include_once('sellyoursaas/class/packages.class.php');
 
 $langs->loadLangs(array("admin","companies","users","contracts","other","commercial","sellyoursaas@sellyoursaas"));
