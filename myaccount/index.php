cf40d99b8980ee1aa3407e5ec2130598fbaf8c99
Hystepik <lmarcouiller@nltechno.com>
2024-08-20 11:07:56 +0200

    fix PR

--- myaccount/index.php
+++ myaccount/index.php
@@ -818,13 +818,13 @@ if ($action == 'updateurl') {	// update URL from the tab "Domain"
 		if ($result) {
 			setEventMessages($langs->trans("TicketSent"), null, 'warnings');
 			$action = '';
-			if (! empty($conf->global->SELLYOURSAAS_DATADOG_ENABLED)) {
+			if (getDolGlobalString("SELLYOURSAAS_DATADOG_ENABLED")) {
 				try {
 					dol_include_once('/sellyoursaas/core/includes/php-datadogstatsd/src/DogStatsd.php');
 	
 					$arrayconfig=array();
-					if (! empty($conf->global->SELLYOURSAAS_DATADOG_APIKEY)) {
-						$arrayconfig=array('apiKey' => $conf->global->SELLYOURSAAS_DATADOG_APIKEY, 'app_key' => $conf->global->SELLYOURSAAS_DATADOG_APPKEY);
+					if (getDolGlobalString("SELLYOURSAAS_DATADOG_APIKEY")) {
+						$arrayconfig=array('apiKey' => getDolGlobalString("SELLYOURSAAS_DATADOG_APIKEY"), 'app_key' => getDolGlobalString("SELLYOURSAAS_DATADOG_APPKEY"));
 					}
 	
 					$statsd = new DataDog\DogStatsd($arrayconfig);
