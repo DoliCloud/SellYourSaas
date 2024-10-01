b15c4f62aff2d3f5b91cc2918addfbf245b6aee0
Laurent Destailleur <eldy@destailleur.fr>
2024-10-01 16:07:22 +0200

    NEW Add label on deployment server

--- class/deploymentserver.class.php
+++ class/deploymentserver.class.php
@@ -117,6 +117,7 @@ class Deploymentserver extends CommonObject
 	public $fields=array(
 		'rowid' => array('type'=>'integer', 'label'=>'TechnicalID', 'enabled'=>'1', 'position'=>1, 'notnull'=>1, 'visible'=>-1, 'noteditable'=>'1', 'index'=>1, 'css'=>'left', 'comment'=>"Id"),
 		'ref' => array('type'=>'varchar(128)', 'label'=>'SellYourSaasSubDomainName', 'enabled'=>'1', 'position'=>10, 'notnull'=>1, 'index'=>1, 'searchall'=>1, 'comment'=>'Reference of object', 'csslist'=>'tdoverflowmax250', 'visible'=>1, 'showoncombobox'=>1),
+		'label' => array('type'=>'varchar(64)', 'label'=>'Label', 'enabled'=>'1', 'position'=>11, 'comment'=>'Label to show incombo list on registration page', 'csslist'=>'tdoverflowmax250', 'visible'=>1, 'showoncombobox'=>1),
 		'fromdomainname' => array('type'=>'varchar(128)', 'label'=>'FromDomainName', 'enabled'=>'1', 'position'=>12, 'notnull'=>0, 'visible'=>1, 'help'=>"FromDomainNameInfo", 'csslist'=>'tdoverflowmax100'),
 		'servercountries' => array('type'=>'varchar(256)', 'label'=>'ServedCountriesServer', 'enabled'=>'1', 'position'=>13, 'notnull'=>0,'visible'=>1, 'help'=>'ServedCountriesServerInfo', 'csslist'=>'tdoverflowmax100'),
 		'entity' =>array('type'=>'integer', 'label'=>'Entity', 'default'=>1, 'enabled'=>1, 'visible'=>0, 'notnull'=>1, 'position'=>20, 'index'=>1),
