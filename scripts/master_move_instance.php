adaadaba3b9425afa599652daaead5f24442e4c5
ldestailleur <eldy@destailleur.fr>
2025-08-11 03:42:30 +0200

    Fix sql syntax

--- scripts/master_move_instance.php
+++ scripts/master_move_instance.php
@@ -926,8 +926,8 @@ $sqlb.= ' WHERE fk_source = '.((int) $oldobject->id)." AND sourcetype = 'contrat
 $sqlc = 'UPDATE '.MAIN_DB_PREFIX.'element_element SET fk_target = '.((int) $newobject->id);
 $sqlc.= ' WHERE fk_target = '.((int) $oldobject->id)." AND targettype = 'contrat' AND (sourcetype = 'facturerec' OR sourcetype = 'facture');";
 
-$sqld = 'UPDATE '.MAIN_DB_PREFIX."contrat_extrafields SET suspendmaintenance_message = '".$dbmaster->escape('https://'.$newobject->ref_customer)."';";
-$sqld.= ' WHERE fk_object = '.((int) $oldobject->id);
+$sqld = 'UPDATE '.MAIN_DB_PREFIX."contrat_extrafields SET suspendmaintenance_message = '".$dbmaster->escape('https://'.$newobject->ref_customer)."'";
+$sqld.= ' WHERE fk_object = '.((int) $oldobject->id).";";
 
 if ($return_var) {
 	print "-> Error during mysql load of instance ".$newobject->ref_customer."\n";
