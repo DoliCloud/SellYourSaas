4169b7ec66317e103786c39a50c6fe0d6159675a
Regis Houssin <regis.houssin@inodbox.com>
2026-09-21 14:52:14 +0200

    clean.sh: log why the cp+rm archive fallback was taken

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -467,6 +467,7 @@ if [ -s /tmp/osutoclean ]; then
 					if [[ $testorconfirm == "confirm" ]]; then
 						mv -f $targetdir/$osusername $archivedirtest 2>/dev/null
 						if [ -d "$targetdir/$osusername" ]; then
+							echo mv failed or left the source dir behind, falling back to cp -pr + rm
 							echo cp -pr $targetdir/$osusername $archivedirtest
 							cp -pr $targetdir/$osusername $archivedirtest
 							rm -fr $targetdir/$osusername
